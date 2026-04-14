e66e1c42f5190254eb6185da5ba0bb1358079497
polinabreha <polinabreha@gmail.com>
2026-04-14 08:28:48 -0400

    added trim

--- src/main/java/com/pluralsight/FullNamePasterApp.java
+++ src/main/java/com/pluralsight/FullNamePasterApp.java
@@ -13,26 +13,26 @@ public class FullNamePasterApp {
 
 
         // separate/split  the string with space
-        String[] parsedFullName = fullName.split(Pattern.quote(" "));
+        String[] parsedFullName = fullName.trim().split(Pattern.quote(" "));
 
        // if it's only first and last name
         // and also the input will convert in an appropriate look
         if (parsedFullName.length == 2) {
             String firstName = Character.toUpperCase(parsedFullName[0].charAt(0)) +
-                    parsedFullName[0].substring(1).toLowerCase().trim();
+                    parsedFullName[0].substring(1).toLowerCase();
             String lastName = Character.toUpperCase(parsedFullName[1].charAt(0)) +
-                    parsedFullName[1].substring(1).toLowerCase().trim();
+                    parsedFullName[1].substring(1).toLowerCase();
             System.out.println("First name: " + firstName);
             System.out.println("Last name: " + lastName);
          // for the full name with middle
             // and also will convert in an appropriate way
         } else if (parsedFullName.length == 3) {
             String firstName = Character.toUpperCase(parsedFullName[0].charAt(0)) +
-                    parsedFullName[0].substring(1).toLowerCase().trim();
+                    parsedFullName[0].substring(1).toLowerCase();
             String lastName = Character.toUpperCase(parsedFullName[2].charAt(0)) +
-                    parsedFullName[2].substring(1).toLowerCase().trim();
+                    parsedFullName[2].substring(1).toLowerCase();
             String middleName = Character.toUpperCase(parsedFullName[1].charAt(0)) +
-                    parsedFullName[1].substring(1).toLowerCase().trim();
+                    parsedFullName[1].substring(1).toLowerCase();
             System.out.println("First name: " + firstName);
             System.out.println("Middle name: " + middleName);
             System.out.println("Last name: " + lastName);
